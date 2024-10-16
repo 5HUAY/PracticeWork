b458b7c006916aa03ff0b3e11eb3e3fb8a02b03c
5HUAY <a.yuzhankov@e-u.edu.ua>
2024-10-16 21:09:25 +0300

    Update Figure1.rs

--- PracticeFile2/Figure1.rs
+++ PracticeFile2/Figure1.rs
@@ -1,1 +1,34 @@
+fn main() 
+{
+    let SIZE: i32 = 5;
 
+    for i in 0..SIZE 
+    {
+        for y in 0..SIZE - i - 1
+        {
+            print!(" ");
+        }
+        
+        for z in 0..2 * i + 1
+        {
+            print!("*");
+        }
+        
+        println!();
+    }
+
+    for i in (0..SIZE - 1).rev() 
+    {
+        for y in 0..SIZE - i - 1
+        {
+            print!(" ");
+        }
+        
+        for z in 0..2 * i + 1
+        {
+            print!("*");
+        }
+        
+        println!();
+    }
+}
