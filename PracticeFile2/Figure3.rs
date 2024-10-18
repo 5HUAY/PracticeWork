437610398d5f636976b9b6b834762ae7700b7e01
5HUAY <a.yuzhankov@e-u.edu.ua>
2024-10-18 10:13:33 +0300

    Create Figure3.rs

---
+++ PracticeFile2/Figure3.rs
@@ -0,0 +1,27 @@
+fn draw_tree(levels: usize) {
+    (0..levels).for_each(|i| {
+        let height = i + 3; // Висота кожного трикутника
+        let max_width = height * 2 - 1;
+
+        (0..height).for_each(|j| {
+            let stars = j * 2 + 1;
+            let spaces = (max_width - stars) / 2;
+            println!("{}{}", " ".repeat(spaces), "*".repeat(stars));
+        });
+    });
+
+    // Малюємо нижню частину (стовбур)
+    let trunk_width = if levels % 2 == 0 { levels + 1 } else { levels }; // Стовбур непарної ширини
+    let trunk_height = levels; // Висота стовбура рівна кількості трикутників
+    let max_width = (levels + 2) * 2 - 1; // Максимальна ширина основи ялинки
+
+    (0..trunk_height).for_each(|_| {
+        let spaces = (max_width - trunk_width) / 2;
+        println!("{}{}", " ".repeat(spaces), "*".repeat(trunk_width));
+    });
+}
+
+fn main() {
+    let triangles = 4; // Кількість трикутників задається тут
+    draw_tree(triangles);
+}
